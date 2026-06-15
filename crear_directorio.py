015da9b9127ca5d6a9b113740a985f242d76be93
Paul Ricardo Maguiña Quispe <paul.maguina@utec.edu.pe>
2026-06-14 22:42:43 -0500

    Enhance lambda_handler to include response details
    Added response details to the S3 put_object call.

--- crear_directorio.py
+++ crear_directorio.py
@@ -1,13 +1,23 @@
 import boto3
 
 def lambda_handler(event, context):
+
+    
     nombre_bucket = event['body']['bucket']
     nombre_directorio = event['body']['directorio']
-    
-    s3 = boto3.client('s3')
-    s3.put_object(Bucket=nombre_bucket, Key=f'{nombre_directorio}/')
 
+
+    s3 = boto3.client('s3')
+
+    response = s3.put_object(
+        Bucket=nombre_bucket,
+        Key=nombre_directorio + '/'
+    )
+
+ 
     return {
         'statusCode': 200,
-        'mensaje': f'Directorio {nombre_directorio}/ creado en {nombre_bucket}'
+        'bucket': nombre_bucket,
+        'directorio': nombre_directorio,
+        'response': str(response)
     }
